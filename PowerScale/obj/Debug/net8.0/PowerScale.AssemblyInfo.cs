148eb474d9797830f4aaf8ad483b1759aa667903
firetrend <pavelananan6@gmail.com>
2026-05-16 00:19:13 +0300

    Sec

--- PowerScale/obj/Debug/net8.0/PowerScale.AssemblyInfo.cs
+++ PowerScale/obj/Debug/net8.0/PowerScale.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PowerScale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b40fcf42fd3d6afd8a0feaa19f548bda32f1ae19")]
 [assembly: System.Reflection.AssemblyProductAttribute("PowerScale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PowerScale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
